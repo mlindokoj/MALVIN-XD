76a42f15726e2244d8121f19f7e167ad099e9256
mlindokoj <johnmlindoko748@gmail.com>
2025-06-10 06:56:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~hncWCBpR#ETQS-VrNfC40RMQ-0e4YfS9WtSjEn8LKLblrMPEnCgM",
+SESSION_ID: process.env.SESSION_ID || "malvin~UzsjGIhK#StMnP9jPk_faNdlXAUe549CfHcvre1QILEVGUjN97WU",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "ᴍᴀʟᴠɪɴ-xᴅ",
 // add bot name here for menu
-MODE: process.env.MODE || "private",
+MODE: process.env.MODE || "PUBLIC",
 // make bot public-private-inbox-group 
 
 LINK_WHITELIST: "youtube.com,github.com",
@@ -30,7 +30,7 @@ LINK_WHITELIST: "youtube.com,github.com",
         AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 
-        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY MALVIN XD 😆*",
+        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY JOHN 😆*",
 // set the auto reply massage on status reply  
 
 
